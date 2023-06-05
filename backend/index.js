ee2c60055f160a6bfd132c40d3044fe1392ddb27
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-06-05 22:50:56 +0000

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDb"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
